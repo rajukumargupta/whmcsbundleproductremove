df0ee9f86f3a666232b30562d813c6e8d185e756
raju <raju.shinedezign@gmail.com>
2018-07-17 10:04:51 +0530

    Update removebundleproduct.php

--- removebundleproduct.php
+++ removebundleproduct.php
@@ -2,7 +2,7 @@
 if ($_GET['a'] == 'remove') {
     $bundleproduct = $_SESSION['cart']['products'][$_GET['i']]['bnum'];
     foreach ($_SESSION['cart']['products'] as $key => $products) {
-        if ($bundleproduct == $products['bnum']) {
+        if (isset($products['bnum']) && $bundleproduct === $products['bnum']) {
             unset($_SESSION['cart']['products'][$key]);
         }
     }
